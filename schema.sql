7bf63e38f2aafa0f8d9831d9578f93033c9c07eb
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-05-06 17:12:29 -0400

    Add schema.sql file to create ERD

---
+++ schema.sql
@@ -0,0 +1,46 @@
+-- Create Title table
+CREATE TABLE titles (
+    title_id        VARCHAR(5)      PRIMARY KEY    NOT NULL,
+    title           VARCHAR(50)     NOT NULL
+);
+
+-- Create Departments table
+CREATE TABLE departments (
+    dept_no        VARCHAR(4)       PRIMARY KEY    NOT NULL,
+    dept_name      VARCHAR(50)                     NOT NULL
+);
+
+-- Create Employees table
+CREATE TABLE employees (
+    emp_no          INT             PRIMARY KEY    NOT NULL,
+    emp_title_id    VARCHAR(5)                     NOT NULL,
+    birth_date      DATE                           NOT NULL,
+    first_name      VARCHAR(16)                    NOT NULL,
+    last_name       VARCHAR(16)                    NOT NULL,
+    sex             VARCHAR(1)                     NOT NULL,
+    hire_date       DATE                           NOT NULL,
+    FOREIGN KEY(emp_title_id) REFERENCES titles(title_id)    
+);
+
+CREATE TABLE dept_emp (
+    emp_no        INT                               NOT NULL,
+    dept_no       VARCHAR(4)                        NOT NULL,
+    PRIMARY KEY(emp_no, dept_no),
+    FOREIGN KEY(emp_no) REFERENCES employees(emp_no),    
+    FOREIGN KEY(dept_no) REFERENCES departments(dept_no)   
+);
+
+CREATE TABLE dept_manager (
+    dept_no       VARCHAR(4)                        NOT NULL,
+    emp_no        INT                               NOT NULL,
+    PRIMARY KEY(dept_no, emp_no),
+    FOREIGN KEY(emp_no) REFERENCES employees(emp_no),    
+    FOREIGN KEY(dept_no) REFERENCES departments(dept_no)   
+);
+
+CREATE TABLE salaries (
+    emp_no       INT                               NOT NULL,
+    salary       INT                                NOT NULL,
+    PRIMARY KEY(emp_no, salary),
+    FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
