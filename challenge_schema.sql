b5897ecb9ec405dabddeace4b932a18bdb886082
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-05-06 22:16:38 -0400

    Importing CSV files to SQL tables

--- challenge_schema.sql
+++ challenge_schema.sql
@@ -1,3 +1,11 @@
+--Drop any existing tables with the names to use for the challenge
+DROP TABLE if exists titles;
+DROP TABLE if exists departments;
+DROP TABLE if exists employees;
+DROP TABLE if exists dept_emp;
+DROP TABLE if exists dept_manager;
+DROP TABLE if exists salaries;
+
 -- Create Title table
 CREATE TABLE titles (
     title_id        VARCHAR(5)      PRIMARY KEY    NOT NULL,
@@ -19,17 +27,19 @@ CREATE TABLE employees (
     last_name       VARCHAR(16)                    NOT NULL,
     sex             VARCHAR(1)                     NOT NULL,
     hire_date       DATE                           NOT NULL,
-    FOREIGN KEY(emp_title_id) REFERENCES titles(title_id)    
+    FOREIGN KEY(emp_title_id)	REFERENCES titles(title_id)    
 );
 
+-- Create Department Employees table
 CREATE TABLE dept_emp (
     emp_no        INT                               NOT NULL,
     dept_no       VARCHAR(4)                        NOT NULL,
     PRIMARY KEY(emp_no, dept_no),
-    FOREIGN KEY(emp_no) REFERENCES employees(emp_no),    
-    FOREIGN KEY(dept_no) REFERENCES departments(dept_no)   
+    FOREIGN KEY(emp_no) 		REFERENCES employees(emp_no),    
+    FOREIGN KEY(dept_no) 		REFERENCES departments(dept_no)   
 );
 
+-- Create Department Managers table
 CREATE TABLE dept_manager (
     dept_no       VARCHAR(4)                        NOT NULL,
     emp_no        INT                               NOT NULL,
@@ -38,9 +48,13 @@ CREATE TABLE dept_manager (
     FOREIGN KEY(dept_no) REFERENCES departments(dept_no)   
 );
 
+-- Create Salaries table
 CREATE TABLE salaries (
     emp_no       INT                               NOT NULL,
-    salary       INT                                NOT NULL,
+    salary       INT                               NOT NULL,
     PRIMARY KEY(emp_no, salary),
     FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
+
+SELECT * 
+FROM dept_emp;
